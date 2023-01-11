89272dd3a82c16a5a2558a819b7961a094e6a4a3
WashingtonAlbuquerque <washington.albuquerqu@gmail.com>
2023-01-10 22:34:56 -0300

    Primeiro Commit

---
+++ js/script.js
@@ -0,0 +1,63 @@
+const inputs = document.querySelectorAll(".input");
+const button = document.querySelector(".login__button")
+const input =  document.querySelector("#viego")
+
+const handleFocus = ({ target }) => {
+    const span = target.previousElementSibling;
+    span.classList.add("span-active")
+}
+
+const handleFocusout = ({target}) => {
+    if (target.value === ""){
+    const span = target.previousElementSibling;
+    span.classList.remove("span-active");
+}
+}
+
+const handleChange = () =>{
+    const [username, password] = inputs;
+
+    if (username.value && password.value.length >= 8){
+    button.removeAttribute("disabled");
+
+} else {
+    button.setAttribute('disabled', '');
+  }
+}
+
+
+inputs.forEach((input) => input.addEventListener("focus", handleFocus));
+inputs.forEach((input) => input.addEventListener("focusout", handleFocusout));
+
+inputs.forEach((input) => input.addEventListener("input", handleChange));
+inputs.forEach((input) => input.addEventListener("input", handleChange));
+
+
+var audio = document.getElementById("viego");
+
+function pauseAudio(){
+    audio.muted = true;
+    
+}
+
+
+
+setTimeout(function(){
+    document.getElementById("viego5").play();
+    
+  }, 40000)
+
+
+
+
+
+/*
+ minhaMusica = new Audio();
+
+
+/*
+minhaMusica.src = './music/viego.mp3'
+minhaMusica.currentTime = 0;
+minhaMusica.play();
+
+*/
